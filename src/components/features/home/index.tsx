b62baefc12ac7a0fc27d2ed95000105be0deca8e
Ahmed Galal <ahmed.galal@propertiano.com>
2025-07-14 20:39:47 +0300

    start game

--- src/components/features/home/index.tsx
+++ src/components/features/home/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
            const { t } = useTranslation()
 
     const [progress, setProgress] = useState<UserProgressType>({
-        currentFlow: FLOW_ENUM.SHOW_VALORIA_MAP,
+        currentFlow: FLOW_ENUM.START_GAME,
         selectedWayIn: null,
         manPower: { army: 90, money: 90, people: 90 }
     })
